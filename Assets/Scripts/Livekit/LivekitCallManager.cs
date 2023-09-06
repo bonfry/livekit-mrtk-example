018b2837477afb017f595ea337360d5a4fd14701
bonfry <info@bonfry.com>
2023-09-06 12:52:51 +0200

    refactor

--- Assets/Scripts/Livekit/LivekitCallManager.cs
+++ Assets/Scripts/Livekit/LivekitCallManager.cs
@@ -7,17 +7,33 @@ namespace Livekit
     public class LivekitCallManager : MonoBehaviour
     {
         [SerializeField] private string url;
-        [SerializeField] private string token;
+
+        [SerializeField] private GameObject selectUserUI;
+
+        [SerializeField] private string userToken1;
+        [SerializeField] private string userToken2;
 
         [SerializeField] RTCEngine rtcEngine;
 
-        public void InitWithToken(string token)
+        private void InitWithToken(string token)
         {
             rtcEngine.Connect(new RTCEngine.ConnectParams
             {
                 url = url,
                 token = token
             });
+        }
+
+        public void InitWithUser1()
+        {
+            selectUserUI.SetActive(false);
+            InitWithToken(userToken1);
+        }
+
+        public void InitWithUser2()
+        {
+            selectUserUI.SetActive(false);
+            InitWithToken(userToken2);
         }
     }
 }
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Livekit MRTK WebRTC Example
+
+This project implements Livekit WebRTC communication using MRTK WebRTC package and [webtick-unity](https://github.com/chef-studios-inc/webtick-unity/)'s Livekit Signaler.
+
+
+
+## Specs 
+ - Unity version: 2021.3.24f1
+ - MRTK WebRTC package version: 2.0
